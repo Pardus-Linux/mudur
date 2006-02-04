1a7343788db21b59f1edddfc7d5abd2471926825
Gürer Özen <gurer@pardus.org.tr>
2006-02-04 22:44:14 +0000

    pathler full olmalı

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-hav register System.Service dbus dbus-serv.py
-hav register System.Service hal hald-serv.py
-hav register System.Service xorg xdm-serv.py
-hav register System.Service alsa-utils alsa-serv.py
-hav register System.Service openssh ssh-serv.py
-hav register System.Service zemberek-server zemberek-serv.py
-hav register System.Service hotplug coldplug-serv.py
+hav register System.Service dbus `pwd`/dbus-serv.py
+hav register System.Service hal `pwd`/hald-serv.py
+hav register System.Service xorg `pwd`/xdm-serv.py
+hav register System.Service alsa-utils `pwd`/alsa-serv.py
+hav register System.Service openssh `pwd`/ssh-serv.py
+hav register System.Service zemberek-server `pwd`/zemberek-serv.py
+hav register System.Service hotplug `pwd`/coldplug-serv.py
 cp mudur.py /sbin/mudur.py
