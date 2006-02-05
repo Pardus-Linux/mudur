20a1ba1a175dd80d074935450e717ce38b3b77a8
S.Çağlar Onur <caglar@pardus.org.tr>
2006-02-05 00:05:57 +0000

    cupsd

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ hav register System.Service alsa-utils `pwd`/alsa-serv.py
 hav register System.Service openssh `pwd`/ssh-serv.py
 hav register System.Service zemberek-server `pwd`/zemberek-serv.py
 hav register System.Service hotplug `pwd`/coldplug-serv.py
+hav register System.Service cupsd `pwd`/cupsd-serv.py
 cp mudur.py /sbin/mudur.py
