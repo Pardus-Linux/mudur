619d32714491ad93be0f981858ce2c983dec494e
Gürer Özen <gurer@pardus.org.tr>
2006-02-07 22:05:28 +0000

    bunlar paket isimleri olmak zorunda ki paketlerle birlikte güncellenebilsin

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 hav register System.Service acpid `pwd`/acpid-serv.py
 hav register System.Service alsa-utils `pwd`/alsa-serv.py
 hav register System.Service cpufreqd `pwd`/cpufreqd-serv.py
-hav register System.Service cupsd `pwd`/cupsd-serv.py
+hav register System.Service cups `pwd`/cupsd-serv.py
 hav register System.Service cron `pwd`/cron-serv.py
 hav register System.Service dbus `pwd`/dbus-serv.py
 hav register System.Service hal `pwd`/hald-serv.py
 hav register System.Service hotplug `pwd`/coldplug-serv.py
 hav register System.Service local `pwd`/local-serv.py
-hav register System.Service logger `pwd`/sysklogd-serv.py
+hav register System.Service sysklogd `pwd`/sysklogd-serv.py
 hav register System.Service openssh `pwd`/ssh-serv.py
 hav register System.Service xorg `pwd`/xdm-serv.py
 hav register System.Service zemberek-server `pwd`/zemberek-serv.py
