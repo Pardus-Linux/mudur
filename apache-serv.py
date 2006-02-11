b62d44143b7853b21b7435c3598b1c9fd20d4206
Ahmet Aygün <ahmet@pardusman.org>
2006-02-11 01:17:48 +0000

    apache de çalışsın

---
+++ apache-serv.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+
+def run(*cmd):
+    """Run a command without running a shell"""
+    return subprocess.call(cmd)
+#
+def get_state():
+    s = get_profile("System.Service.setState")
+    if s:
+        state = s["state"]
+    else:
+        state = "on"
+    return state
+#
+def info():
+    state = get_state()
+    return "local\n" + state + "\nApache Web Server"
+
+def start():
+    run("/usr/sbin/apachec2tl", "-d", "/usr/lib/apache2/", "-f", "/etc/apache2/httpd.conf", get_config_vars(), "-k", "start")
+
+def stop():
+    run("/usr/sbin/apachec2tl", "-d", "/usr/lib/apache2/", "-f", "/etc/apache2/httpd.conf", get_config_vars(), "-k", "stop")
+
+def ready():
+    s = get_state()
+    if s == "on":
+        start()
+
+def setState(state=None):
+    if state == "on":
+        start()
+    elif state == "off":
+        stop()
+    else:
+        fail("Unknown state '%s'" % state)
+
+def get_config_vars():
+    return map(lambda x: x.split('=')[1].strip().strip('"'), [line for line in open('/etc/conf.d/apache2').readlines() if line.strip().startswith('APACHE2_OPTS')])[0]
