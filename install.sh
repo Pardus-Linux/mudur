eb315ec35459c9b7e6fb8b94943d58d077e74474
S.Çağlar Onur <caglar@pardus.org.tr>
2006-02-28 21:52:33 +0000

    betikler scripts altına gitsin, karışık görünmesin

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-hav register System.Service acpid `pwd`/acpid-serv.py
-hav register System.Service alsa-utils `pwd`/alsa-serv.py
-hav register System.Service cpufreqd `pwd`/cpufreqd-serv.py
-hav register System.Service cups `pwd`/cupsd-serv.py
-hav register System.Service cron `pwd`/cron-serv.py
-hav register System.Service dbus `pwd`/dbus-serv.py
-hav register System.Service hal `pwd`/hald-serv.py
-hav register System.Service hotplug `pwd`/coldplug-serv.py
-hav register System.Service local `pwd`/local-serv.py
-hav register System.Service mdnsd `pwd`/mdnsd-serv.py
-hav register System.Service sysklogd `pwd`/sysklogd-serv.py
-hav register System.Service openssh `pwd`/ssh-serv.py
-hav register System.Service xorg `pwd`/xdm-serv.py
-hav register System.Service zemberek-server `pwd`/zemberek-serv.py
+hav register System.Service acpid `pwd`/scripts/acpid-serv.py
+hav register System.Service alsa-utils `pwd`/scripts/alsa-serv.py
+hav register System.Service cpufreqd `pwd`/scripts/cpufreqd-serv.py
+hav register System.Service cups `pwd`/scripts/cupsd-serv.py
+hav register System.Service cron `pwd`/scripts/cron-serv.py
+hav register System.Service dbus `pwd`/scripts/dbus-serv.py
+hav register System.Service hal `pwd`/scripts/hald-serv.py
+hav register System.Service hotplug `pwd`/scripts/coldplug-serv.py
+hav register System.Service local `pwd`/scripts/local-serv.py
+hav register System.Service mdnsd `pwd`/scripts/mdnsd-serv.py
+hav register System.Service sysklogd `pwd`/scripts/sysklogd-serv.py
+hav register System.Service openssh `pwd`/scripts/ssh-serv.py
+hav register System.Service xorg `pwd`/scripts/xdm-serv.py
+hav register System.Service zemberek-server `pwd`/scripts/zemberek-serv.py
 cp mudur.py /sbin/mudur.py
 cp service.py /bin/service
