39c443bfd1218485868ea70e2a02f1e8b8185d6e
Gürer Özen <gurer@pardus.org.tr>
2006-03-04 11:52:39 +0000

    fix denemesi dursun gene

--- scripts/hald-serv.py
+++ scripts/hald-serv.py
@@ -1,9 +1,23 @@
 import os
+import socket
 import subprocess
+import time
 
 def run(*cmd):
     """Run a command without running a shell"""
     return subprocess.call(cmd)
+
+def wait_for_bus(unix_name, retry=10, wait=0.2):
+    sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
+    count = 0
+    while count < retry:
+        try:
+            sock.connect(unix_name)
+            return True
+        except:
+            count += 1
+        time.sleep(wait)
+    return False
 
 #
 
@@ -25,6 +39,7 @@ def info():
 def start():
     call("System.Service.start", "dbus")
     call("System.Service.start", "acpid")
+    wait_for_bus("/var/lib/dbus/system_bus_socket")
     run("/sbin/start-stop-daemon", "--start", "-q",
         "--exec", "/usr/sbin/hald", "--", "--retain-privileges")
 
