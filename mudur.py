4875f969f63f4c81d635414006fe37ad9ef9692a
Barış Metin <baris@pardus.org.tr>
2006-03-06 21:35:44 +0000

    uludag -> pardus

--- mudur.py
+++ mudur.py
@@ -554,7 +554,7 @@ os.environ["PATH"] = "/bin:/sbin:/usr/bin:/usr/sbin:" + os.environ["PATH"]
 if sys.argv[1] == "sysinit":
     # This is who we are
     print "\x1b%G"
-    print "Pardus, http://www.uludag.org.tr"
+    print "Pardus, http://www.pardus.org.tr"
     print
     
     # mount /proc
