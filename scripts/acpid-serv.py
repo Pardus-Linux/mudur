b3d74daac6463084e76e6c07b4f12973b3aaf3a8
S.Çağlar Onur <caglar@pardus.org.tr>
2006-03-09 18:27:16 +0000

    temizlik/güzellik

--- scripts/acpid-serv.py
+++ scripts/acpid-serv.py
@@ -1,53 +1,19 @@
-
+from comar.service import *
 import os
-import subprocess
 
-def run(*cmd):
-    """Run a command without running a shell"""
-    return subprocess.call(cmd)
-
-#
+serviceType = "local"
+serviceDesc = "ACPID"
 
 def check_config():
     if not os.path.exists("/proc/acpi"):
         fail("ACPI support has not been compiled into the kernel")
-#
-
-def get_state():
-    s = get_profile("System.Service.setState")
-    if s:
-        state = s["state"]
-    else:
-        state = "on"
-    
-    return state
-
-#
-
-def info():
-    state = get_state()
-    return "local\n" + state + "\nACPID"
 
 def start():
     check_config()
-    run("/sbin/start-stop-daemon", "--start", "--quiet", "--exec", "/usr/sbin/acpid", "--", "-c", "/etc/acpi/events")
+    run("/sbin/start-stop-daemon --start --quiet --exec /usr/sbin/acpid -- -c /etc/acpi/events")
 
 def stop():
-    run("/sbin/start-stop-daemon", "--stop", "--quiet", "--exec", "/usr/sbin/acpid")
+    run("/sbin/start-stop-daemon --stop --quiet --exec /usr/sbin/acpid")
 
-#FIXME: Gürer reload ekleyince
-#def reload():
-#    run("/sbin/start-stop-daemon", "--stop", "--quiet", "--exec", "/usr/sbin/acpid", "--signal", "HUP")
-
-def ready():
-    s = get_state()
-    if s == "on":
-        start()
-
-def setState(state=None):
-    if state == "on":
-        start()
-    elif state == "off":
-        stop()
-    else:
-        fail("Unknown state '%s'" % state)
+def reload():
+    run("/sbin/start-stop-daemon --stop --quiet --exec /usr/sbin/acpid --signal HUP")
--- scripts/cron-serv.py
+++ scripts/cron-serv.py
@@ -1,42 +1,10 @@
-import os
-import subprocess
+from comar.service import *
 
-def run(*cmd):
-    """Run a command without running a shell"""
-    return subprocess.call(cmd)
-
-#
-
-def get_state():
-    s = get_profile("System.Service.setState")
-    if s:
-        state = s["state"]
-    else:
-        state = "on"
-    
-    return state
-
-#
-
-def info():
-    state = get_state()
-    return "local\n" + state + "\nVixie Cron"
+serviceType = "local"
+serviceDesc = "Cron"
 
 def start():
-    run("start-stop-daemon", "--start", "--quiet", "--exec", "/usr/sbin/cron")
+    run("start-stop-daemon --start --quiet --exec /usr/sbin/cron")
 
 def stop():
-    run("start-stop-daemon", "--stop", "--quiet", "--pidfile", "/var/run/cron.pid")
-    
-def ready():
-    s = get_state()
-    if s == "on":
-        start()
-
-def setState(state=None):
-    if state == "on":
-        start()
-    elif state == "off":
-        stop()
-    else:
-        fail("Unknown state '%s'" % state)
+    run("start-stop-daemon --stop --quiet --pidfile /var/run/cron.pid")
