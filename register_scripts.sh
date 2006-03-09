e83ddd6b86e37aed498e5f49f577acc33e40de25
S.Çağlar Onur <caglar@pardus.org.tr>
2006-03-09 18:55:59 +0000

    yeni isimleri kullan

--- register_scripts.sh
+++ register_scripts.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-hav register System.Service acpid `pwd`/scripts/acpid-serv.py
-hav register System.Service alsa-utils `pwd`/scripts/alsa-serv.py
-hav register System.Service cpufreqd `pwd`/scripts/cpufreqd-serv.py
-hav register System.Service cups `pwd`/scripts/cupsd-serv.py
-hav register System.Service cron `pwd`/scripts/cron-serv.py
-hav register System.Service dbus `pwd`/scripts/dbus-serv.py
-hav register System.Service hal `pwd`/scripts/hald-serv.py
-hav register System.Service local `pwd`/scripts/local-serv.py
-hav register System.Service mdnsd `pwd`/scripts/mdnsd-serv.py
-hav register System.Service sysklogd `pwd`/scripts/sysklogd-serv.py
-hav register System.Service openssh `pwd`/scripts/ssh-serv.py
-hav register System.Service powersave `pwd`/scripts/powersave-serv.py
-hav register System.Service xorg `pwd`/scripts/xdm-serv.py
-hav register System.Service zemberek-server `pwd`/scripts/zemberek-serv.py
+!/bin/bash
+hav register System.Service acpid `pwd`/scripts/acpid.py
+hav register System.Service alsa-utils `pwd`/scripts/alsa.py
+hav register System.Service cpufreqd `pwd`/scripts/cpufreqd.py
+hav register System.Service cups `pwd`/scripts/cupsd.py
+hav register System.Service cron `pwd`/scripts/cron.py
+hav register System.Service dbus `pwd`/scripts/dbus.py
+hav register System.Service hal `pwd`/scripts/hald.py
+hav register System.Service local `pwd`/scripts/local.py
+hav register System.Service mdnsd `pwd`/scripts/mdnsd.py
+hav register System.Service sysklogd `pwd`/scripts/sysklogd.py
+hav register System.Service openssh `pwd`/scripts/ssh.py
+hav register System.Service powersave `pwd`/scripts/powersave.py
+hav register System.Service xorg `pwd`/scripts/xdm.py
+hav register System.Service zembereker `pwd`/scripts/zemberek.py
