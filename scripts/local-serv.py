da6661f319cbd419edb40c5a212464f5f7fc41f9
S.Çağlar Onur <caglar@pardus.org.tr>
2006-03-09 18:32:08 +0000

    temizlik/güzellik

--- scripts/local-serv.py
+++ scripts/local-serv.py
@@ -1,26 +1,9 @@
 import os
-import subprocess
+from comar.service import *
 
-def run(*cmd):
-    """Run a command without running a shell"""
-    return subprocess.call(cmd)
+serviceType = "local"
 
-#
-
-def get_state():
-    s = get_profile("System.Service.setState")
-    if s:
-        state = s["state"]
-    else:
-        state = "on"
-    
-    return state
-
-#
-
-def info():
-    state = get_state()
-    return "local\n" + state + "\nCustom startup commands"
+serviceDesc = "Custom startup commands"
 
 def start():
     if os.path.exists("/etc/conf.d/local.start"):
@@ -29,16 +12,3 @@ def start():
 def stop():
     if os.path.exists("/etc/conf.d/local.stop"):
         os.system("source /etc/conf.d/local.stop")
-
-def ready():
-    s = get_state()
-    if s == "on":
-        start()
-
-def setState(state=None):
-    if state == "on":
-        start()
-    elif state == "off":
-        stop()
-    else:
-        fail("Unknown state '%s'" % state)
