b5bc4ac76e2325e314f45acce56df17ffdd4a79d
S.Çağlar Onur <caglar@pardus.org.tr>
2006-03-09 18:29:56 +0000

    temizlik/güzellik

--- scripts/cupsd-serv.py
+++ scripts/cupsd-serv.py
@@ -1,42 +1,10 @@
-import os
-import subprocess
+from comar.service import *
 
-def run(*cmd):
-    """Run a command without running a shell"""
-    return subprocess.call(cmd)
-
-#
-
-def get_state():
-    s = get_profile("System.Service.setState")
-    if s:
-        state = s["state"]
-    else:
-        state = "on"
-    
-    return state
-
-#
-
-def info():
-    state = get_state()
-    return "local\n" + state + "\nCUPS"
+serviceType = "server"
+serviceDesc = "CUPSD"
 
 def start():
-    run("/sbin/start-stop-daemon", "--start", "-q", "--exec", "/usr/sbin/cupsd")
+    run("/sbin/start-stop-daemon --start -q --exec /usr/sbin/cupsd")
 
 def stop():
-    run("/sbin/start-stop-daemon", "--stop", "-q", "--exec", "/usr/sbin/cupsd")
-
-def ready():
-    s = get_state()
-    if s == "on":
-        start()
-
-def setState(state=None):
-    if state == "on":
-        start()
-    elif state == "off":
-        stop()
-    else:
-        fail("Unknown state '%s'" % state)
+    run("/sbin/start-stop-daemon --stop -q --exec /usr/sbin/cupsd")
--- scripts/dbus-serv.py
+++ scripts/dbus-serv.py
@@ -1,6 +1,9 @@
-
 import os
-import subprocess
+from comar.service import *
+
+
+serviceType = "server"
+serviceDesc = "DBUS Message Bus"
 
 def unlink(path):
     try:
@@ -8,45 +11,9 @@ def unlink(path):
     except:
         pass
 
-def run(*cmd):
-    """Run a command without running a shell"""
-    return subprocess.call(cmd)
-
-#
-
-def get_state():
-    s = get_profile("System.Service.setState")
-    if s:
-        state = s["state"]
-    else:
-        state = "on"
-    
-    return state
-
-#
-
-def info():
-    state = get_state()
-    return "local\n" + state + "\nD-BUS System Messagebus"
-
 def start():
-    run("/sbin/start-stop-daemon", "--start", "--pidfile",
-        "/var/run/dbus.pid", "--exec", "/usr/bin/dbus-daemon-1",
-        "--", "--system")
+    run("/sbin/start-stop-daemon --start --pidfile /var/run/dbus.pid --exec /usr/bin/dbus-daemon-1 -- --system")
 
 def stop():
-    run("/sbin/start-stop-daemon", "--stop", "--pidfile", "/var/run/dbus.pid")
+    run("/sbin/start-stop-daemon --stop --pidfile /var/run/dbus.pid")
     unlink("/var/run/dbus.pid")
-
-def ready():
-    s = get_state()
-    if s == "on":
-        start()
-
-def setState(state=None):
-    if state == "on":
-        start()
-    elif state == "off":
-        stop()
-    else:
-        fail("Unknown state '%s'" % state)
