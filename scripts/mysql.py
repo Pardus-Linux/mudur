dc94962666eb01c10fea8fc3ac90c3d785f94e2f
Bahadır Kandemir <bahadir@pardus.org.tr>
2006-03-10 23:52:26 +0000

    fix

--- scripts/mysql.py
+++ scripts/mysql.py
@@ -8,13 +8,12 @@ def check_mysql():
     if not os.path.exists("/var/lib/mysql"):
         fail("MySQL is not installed")
 
-    return "server\n" + state + "\nMySQL DB Server"
-
 def start():
     check_mysql()
-    run("/sbin/start-stop-daemon --start --quiet --background --exec /usr/bin/mysqld_safe -- --user=mysql \
-    --basedir=/usr --datadir=/var/lib/mysql --max_allowed_packet=8M --net_buffer_length=16K \
-    --socket=/var/run/mysqld/mysqld.sock --pid-file=/var/run/mysqld/mysqld.pid")
+    run("/sbin/start-stop-daemon --start --quiet --background --exec /usr/sbin/mysqld -- \
+         --basedir=/usr --datadir=/var/lib/mysql --pid-file=/var/run/mysqld/mysqld.pid \
+         --skip-locking --port=3306 --socket=/var/run/mysqld/mysqld.sock --max_allowed_packet=8M \
+         --net_buffer_length=16K")
 
 def stop():
     run("/sbin/start-stop-daemon --stop --retry 5 --quiet --pidfile=/var/run/mysqld/mysqld.pid")
