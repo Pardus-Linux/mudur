6e2e420bc782be8fd5bd46bbd9a7260999f1f4d3
Gürer Özen <gurer@pardus.org.tr>
2006-03-20 12:36:18 +0000

    hotplug betiğini de kur

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@
 import sys
 import os
 
-source_list = [ "mudur.py", "service.py" ]
+i18n_source_list = [ "mudur.py", "service.py" ]
 
 def update_messages():
-    os.system("xgettext -o po/mudur.pot %s" % " ".join(source_list))
+    os.system("xgettext -o po/mudur.pot %s" % " ".join(i18n_source_list))
     for item in os.listdir("po"):
         if item.endswith(".po"):
             os.system("msgmerge -q -o temp.po po/%s po/mudur.pot" % item)
@@ -29,6 +29,7 @@ def install(args):
         prefix = args[0]
     
     os.system("cp mudur.py %s" % os.path.join(prefix, "sbin/mudur.py"))
+    os.system("cp hotplug.py %s" % os.path.join(prefix, "sbin/hotplug.py"))
     os.system("cp service.py %s" % os.path.join(prefix, "bin/service"))
     
     for item in os.listdir("po"):
