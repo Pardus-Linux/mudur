431a18683b434410b0b1d4750c1a8320f8e53cc1
Gürer Özen <gurer@pardus.org.tr>
2006-06-06 16:02:18 +0000

    biraz daha geliş

--- power-test.py
+++ power-test.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 
+import os
+
+
 class CPU:
     def __init__(self):
         self.vendor = "unknown"
@@ -18,16 +21,53 @@ class CPU:
             elif line.startswith("flags"):
                 self.flags = line.split(":", 1)[1].strip().split()
     
+    def _find_pci(self, vendor, device):
+        path = "/sys/bus/pci/devices"
+        for item in os.listdir(path):
+            ven = file(os.path.join(path, item, "vendor")).read().rstrip("\n")
+            dev = file(os.path.join(path, item, "device")).read().rstrip("\n")
+            if ven == vendor and dev == device:
+                return item
+        return None
+    
+    def _detect_speedstep(self):
+        # FIXME: implement this
+        return 0
+    
+    def _detect_ich(self):
+        ich = 0
+        if self._find_pci("0x8086", "0x24cc"):
+            # ICH4-M
+            ich = 4
+        if self._find_pci("0x8086", "0x248c"):
+            # ICH3-M
+            ich = 3
+        if self._find_pci("0x8086", "0x244c"):
+            # ICH2-M
+            # has trouble with old 82815 host bridge revisions
+            if not self._find_pci("0x8086", "0x"):
+                ich = 2
+        return ich
+    
     def findModules(self):
         modules = set()
         if self.vendor == "GenuineIntel":
+            # Pentium M, Enhanced SpeedStep
             if "est" in self.flags:
                 modules.add("speedstep-centrino")
-            if "acpi" in self.flags and "acc" in self.flags:
+            # P4 and XEON processors with thermal control
+            elif "acpi" in self.flags and "tm" in self.flags:
                 modules.add("p4-clockmod")
+            # SpeedStep
+            elif self._detect_speedstep():
+                # SpeedStep ICH, PIII-M and P4-M with ICH2/3/4 southbridges
+                if self._detect_ich():
+                    modules.add("speedstep-ich")
         elif self.vendor == "AuthenticAMD":
+            # Mobile K6-1/2 CPUs
             if self.family == 5 and (self.model == 12 or self.model == 13):
                 modules.add("powernow-k6")
+            # Mobile Athlon/Duron
             elif self.family == 6:
                 modules.add("powernow-k7")
             #elif lala:
