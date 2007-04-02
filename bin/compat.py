5c773d112fc69d4786150dd44b47189c37554aa5
Gürer Özen <gurer@pardus.org.tr>
2007-04-02 13:53:53 +0000

    lsb uyumluluk için ufak betik

---
+++ bin/compat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version. Please read the COPYING file.
+#
+
+import sys
+import os
+
+def wrap_service(package, op):
+    os.system("service %s %s" % (package, op))
+
+if __name__ == "__main__":
+    wrap_service(os.path.basename(sys.argv[0]), sys.argv[1])
