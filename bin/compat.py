b69da375767fb30fbb83176d139f29bcaa610fb4
Gürer Özen <gurer@pardus.org.tr>
2007-04-03 11:57:16 +0000

    init.d altında uyumluluk linkleri oluştur otomatik olarak

--- bin/compat.py
+++ bin/compat.py
@@ -17,5 +17,15 @@ def wrap_service(package, op):
     cmd = ["service", package, op]
     return subprocess.call(cmd)
 
+def populate_initd():
+    for name in os.listdir("/var/db/comar/code"):
+        if name.startswith("System_Service_"):
+            srvname = name[15:-3]
+            os.symlink("compat.py", "/etc/init.d/%s" % srvname)
+
 if __name__ == "__main__":
-    sys.exit(wrap_service(os.path.basename(sys.argv[0]), sys.argv[1]))
+    myname = os.path.basename(sys.argv[0])
+    if len(sys.argv) == 2:
+        sys.exit(wrap_service(myname, sys.argv[1]))
+    elif myname == "compat.py" and os.getuid() == 0:
+        populate_initd()
