9b972a3595d04b3e5a82c8ab0a34ff1af005c9ea
Onur Küçük <onur@pardus.org.tr>
2007-05-20 15:12:10 +0000

    fix console font problems

--- bin/mudur.py
+++ bin/mudur.py
@@ -341,7 +341,7 @@ class Language:
 
 languages = {
     "en": Language("us", "iso01.16", "8859-1", "en_US.UTF-8"),
-    "tr": Language("trq", "iso09.16", "8859-9", "tr_TR.UTF-8"),
+    "tr": Language("trq", "lat5u-16", "8859-9", "tr_TR.UTF-8"),
     "nl": Language("nl", "iso01.16", "8859-1", "nl_NL.UTF-8"),
     "de": Language("de", "iso01.16", "8859-1", "de_DE.UTF-8"),
     "es": Language("es", "iso01.16", "8859-1", "es_ES.UTF-8"),
