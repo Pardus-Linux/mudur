7fb15c80f94826129ba203b799ac7c5559c29607
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-25 06:22:55 +0000

    Fix load_file method. If we don't have any exceptions, data='' line makes variable empty.

--- bin/mudur.py
+++ bin/mudur.py
@@ -56,12 +56,12 @@ def wait_bus(unix_name, timeout=5, wait=0.1, stream=True):
 
 def load_file(path, ignore_comments=False):
     """Reads the contents of a file and returns it."""
+    data = ""
     try:
         with open(path, "r") as _file:
             data = _file.read()
     except IOError:
         return ""
-    data = ""
     if ignore_comments:
         data = filter(lambda x: not (x.startswith("#") or x == ""), data)
     return data
